579b71feeaf4d7274ee68f07ce7f9df060d3705b
PeratX <1215714524@qq.com>
2020-07-13 10:21:21 +0800

    mirai-kts: update core

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
     implementation(kotlin("scripting-compiler-impl-embeddable"))
     implementation("org.jetbrains.intellij.deps:trove4j:1.0.20200330")
 
-    implementation("net.mamoe:mirai-core:1.0.0")
+    implementation("net.mamoe:mirai-core:1.1.1")
     implementation("net.mamoe:mirai-console:0.5.2")
 }
 
